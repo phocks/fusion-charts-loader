7b638fe9bb6a9a7aa0dfe2639c86fd75a598209a
Joshua Byrd <phocks@gmail.com>
2017-08-29 16:46:43 +1000

    Aspect ratio stuff. Starting out

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // var FusionCharts = require("fusioncharts");
-var loadJS = require('loadjs');
-var shortid = require('shortid');
+const loadJS = require('loadjs');
+const shortid = require('shortid');
+
+const styles = require('./styles.scss')
 
 // require("fusioncharts/fusioncharts.charts")(FusionCharts);
-
 
 let render = () => {
   ABC.News.embedExternalLinks.registerHandler({
@@ -11,27 +12,52 @@ let render = () => {
     pattern: /\/news\/interactives\/fchart\/\?xml=(.*)&type=(.*)$/,
     mobile: true,
     caption: 'Infographic',
-    callback: function(options) {
+    callback (options) {
+
       // Get the URL so we can extract the params later
       const parametersURL = options.externalLink;
+
+      let chartType =   getParameterByName('type', parametersURL),
+          chartWidth =  getParameterByName('width', parametersURL) || "100%",
+          chartHeight = getParameterByName('height', parametersURL) || "100%",
+          dataFormat =  getParameterByName('format', parametersURL) || "xmlurl",
+          dataSource =  getParameterByName('xml', parametersURL);
+
+
+      // Generate unique ID so we can have multiple charts
       const chartName = "chart-container-" + shortid.generate();
 
-      const rootEl = options.container[0];
-      const appEl = document.createElement('div');
 
-      appEl.innerHTML = '<div id="' + chartName + '">Loading...</div>';
+      const rootEl = options.container[0];
+
+      let rootWidth = rootEl.offsetWidth;
+
+
+      let appEl = document.createElement('div');
+
+      appEl.innerHTML =
+       `<div class=${styles.fusionChart} id="${chartName}">
+          <div class="spinner"></div>
+        </div>`;
+
+      // Replace external link without chart div
       rootEl.innerHTML = appEl.innerHTML;
+
+      // Just select the element again so we can modify it
+      appEl = document.getElementById(chartName);
+      appEl.style.height = rootWidth + "px"; // 1x1 square for now
+
 
       loadJS([ "/cm/code/8851742/fusioncharts.js", "/cm/code/8851862/fusioncharts.charts.js" ], {
         success: () => {
-          FusionCharts.ready(function () {
+          FusionCharts.ready( () => {
             var fusionChart = new FusionCharts({
-              "type": getParameterByName('type', parametersURL),
-              "renderAt": chartName,
-              "width": getParameterByName('width', parametersURL) || "100%",
-              "height": getParameterByName('height', parametersURL) || "500",
-              "dataFormat": getParameterByName('format', parametersURL) || "xmlurl",
-              "dataSource": getParameterByName('xml', parametersURL)
+              "type":       chartType,
+              "renderAt":   chartName,
+              "width":      chartWidth,
+              "height":     chartHeight,
+              "dataFormat": dataFormat,
+              "dataSource": dataSource
             });
             fusionChart.render();
           });
@@ -43,27 +69,6 @@ let render = () => {
 };
 
 
-// Do some hot reload magic with errors
-// if (process.env.NODE_ENV !== 'production' && module.hot) {
-//   // Wrap the actual renderer in an error trap
-//   let renderFunction = render;
-//   render = () => {
-//   try {
-//     renderFunction();
-//   } catch (e) {
-//     // Render the error to the screen in place of the actual app
-//     const ErrorBox = require('./error-box');
-//     Preact.render(<ErrorBox error={e} />, element, element.lastChild);
-//     }
-//   };
-
-//   // If a new app build is detected try rendering it
-//   module.hot.accept('./components/app', () => {
-//   setTimeout(render);
-//   });
-// }
-
-// Optionally change this to wait for Odyssey
 render();
 
 
